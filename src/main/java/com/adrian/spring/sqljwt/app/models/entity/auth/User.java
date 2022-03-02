3ab30c533c43d0e9d0e6ef27217861734503ce69
Adrián Antón <adrtler@gmail.com>
2022-03-02 18:46:02 +0100

    Entidades de usuario y roles, datos de prueba

---
+++ src/main/java/com/adrian/spring/sqljwt/app/models/entity/auth/User.java
@@ -0,0 +1,118 @@
+package com.adrian.spring.sqljwt.app.models.entity.auth;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * Clase entidad de usuario, va a tener las propiedades de los usuarios
+ * logueados en la aplicación
+ * 
+ * @author Adrián
+ *
+ */
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -5201177349674572051L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 45)
+	private String name;
+
+	@Column(nullable = false, length = 60)
+	private String password;
+
+	@Column(unique = true)
+	private String email;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private List<Role> role;
+
+	@Column(columnDefinition = "boolean default true")
+	private Boolean enabled;
+
+	@Column(name = "created_at")
+	@Temporal(TemporalType.DATE)
+	private Date created_at;
+
+	@PrePersist
+	public void prePersist() {
+		created_at = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Role> getRole() {
+		return role;
+	}
+
+	public void setRole(List<Role> role) {
+		this.role = role;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password, enabled) VALUES ('adrian','adrian@gmail.com', '$2a$10$kv6V.xbjPOz7Oobdz.JT3ugX8J7FOLrui0NXM/a50BUzOpJ9kSZem',1);
+INSERT INTO users (name, email, password, enabled) VALUES ('admin','admin@gmail.com', '$2a$10$LeHrq3fIQIRXo4Cf3KaRoeCjIv7f2ichiSe0SMAwDZiqZLdmnlMOu',1);
