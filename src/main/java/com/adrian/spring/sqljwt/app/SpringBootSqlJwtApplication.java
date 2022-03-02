d09d093760132aff9aeafdcafd9cc1a0f487019f
Adrián Antón <adrtler@gmail.com>
2022-03-02 18:07:25 +0100

    Primer commit

---
+++ src/main/java/com/adrian/spring/sqljwt/app/SpringBootSqlJwtApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.spring.sqljwt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqlJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSqlJwtApplication.class, args);
+	}
+
+}
